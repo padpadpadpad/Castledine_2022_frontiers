5da66644e4af760b526e58262818014ea8d014c3
Daniel Padfield <d.padfieldscfc@gmail.com>
2022-02-20 17:17:08 +0000

    Update everything

--- scripts/map_phage_sequencing.sh
+++ scripts/map_phage_sequencing.sh
@@ -87,84 +87,78 @@ trim_galore --paired --quality 10 --output_dir $trimmed_files $wd/ERR7976214_1.f
 # run fastqc on files
 fastqc -o $fastqc_output $trimmed_files/*.fq.gz
 
-# map to reference genome
-
-
 # make directories for bam files
 bam_files=$wd/bams
 mkdir -p $bam_files
 
-ref_phi2=$wd/reference/GCA_000886135.1_ViralProj42717_genomic.fna
+ref_phi2=~/PRJEB50009/ref/GCA_000886135.1_ViralProj42717_genomic.fna
 
 # map reads to reference genome using minimap2
-for file in $trimmed_files/*R1_val_1.fq.gz
-
-    do
-        echo $file
-        
-        # assign fwd and rev files
-        file_fwd=$file
-        file_rev=${file%R1_val_1.fq.gz}R2_val_2.fq.gz
-
-        echo $file_fwd
-        echo $file_rev
-
-        #stub
-        stub=$(basename ${file%_R1_val_1.fq.gz})
-
-        # do the mapping for fwd and reverse pairs
-        # use samblaster to mark duplicates
-        minimap2 -ax sr $ref_phi2 $file_fwd $file_rev | samblaster | samtools view -S -b -o $bam_files/${stub}_minimap2.bam
-
-done
+# use samblaster to mark duplicates
+minimap2 -ax sr $ref_phi2 $trimmed_files/ERR7976214_1_val_1.fq.gz $trimmed_files/ERR7976214_2_val_2.fq.gz | samblaster | samtools view -S -b -o $bam_files/phage_1_minimap2.bam
 
 # make directories for mapped and unmapped files
 mkdir -p $bam_files/mapped
 mkdir -p $bam_files/unmapped
 mkdir -p $bam_files/stats
 
-for bam_file in $bam_files/*.bam
+# 1. split bam into mapped and unmapped reads
+samtools view -b -F4 $bam_files/phage_1_minimap2.bam > $bam_files/mapped/phage_1_mapped.bam
+samtools view -f4 $bam_files/phage_1_minimap2.bam > $bam_files/unmapped/phage_1_unmapped.bam
 
-    do
-        echo $bam_file
-        stub=${bam_file%.bam}
-        stub2=$(basename ${bam_file%.bam})
+# 2. sort mapped file by position in genome and not by order of mapped sequence
+samtools sort -o $bam_files/mapped/phage_1_sorted.bam $bam_files/mapped/phage_1_mapped.bam
 
-        # 1. split bam into mapped and unmapped reads
-        samtools view -b -F4 $bam_file > $bam_files/mapped/${stub2}_mapped.bam
-        samtools view -f4 $bam_file > $bam_files/unmapped/${stub2}_unmapped.bam
+# 3. index the sorted bam file
+samtools index $bam_files/mapped/phage_1_sorted.bam
 
-        # 2. sort mapped file by position in genome and not by order of mapped sequence
-        samtools sort -o $bam_files/mapped/${stub2}_sorted.bam $bam_files/mapped/${stub2}_mapped.bam
+# 4. remove intermediate (unsorted mapped bam file)
+rm $bam_files/mapped/phage_1_mapped.bam
 
-        # 3. index the sorted bam file
-        samtools index $bam_files/mapped/${stub2}_sorted.bam
+# 5. extract stats of file
+samtools flagstat $bam_files/phage_1_minimap2.bam > $bam_files/stats/phage_1_raw_stats.txt
 
-        # 3. remove intermediate (unsorted mapped bam file)
-        rm $bam_files/mapped/${stub2}_mapped.bam
+# run freebayes to call SNPs and indels
+# make directory for output
+mkdir -p $wd/vcf_output
 
-        # 4. extract stats of file
-        samtools flagstat $bam_file > $bam_files/stats/${stub2}_raw_stats.txt
+# call SNPs using FreeBayes, set ploidy to 1
+freebayes -f $ref_phi2  -p 1 $bam_files/mapped/phage_1_sorted.bam > $wd/vcf_output/phage_1.vcf
+# keep only snps with a QUAL score > 20, based on recommendation of Erik Garrison
+#https://www.biostars.org/p/71299/
+vcffilter -f "QUAL > 20" $wd/vcf_output/phage_1.vcf > $wd/vcf_output/phage_1_filter.vcf
 
-done
+#-------------------------------------#
+# map and call variants of phage 2 ####
+#-------------------------------------#
 
-#--------------------------------------------------#
-# run freebayes to call variants for each bam file #
-#--------------------------------------------------#
+# set working directory and create bam folder
+wd=~/PRJEB50009/ERR7976218
+
+mkdir -p $wd/bam_files
+
+cd $wd
+
+# use minimap2 to map long reads
+minimap2 -ax map-pb $ref_phi2 ERR7976218.fastq.gz > $wd/bam_files/phage_2_minimap2.bam
+
+# sort mapped file by position in genome and not by order of mapped sequence
+samtools view -b -F 4 $wd/bam_files/phage_2_minimap2.bam > $wd/bam_files/phage_2_mapped.bam
+samtools sort -o $wd/bam_files/phage_2_sorted.bam $wd/bam_files/phage_2_mapped.bam
+
+# index the sorted bam file
+samtools index $wd/bam_files/phage_2_sorted.bam
+
+# get flagstats
+samtools flagstat $wd/bam_files/phage_2_minimap2.bam > $wd/bam_files/phage_2_stats.txt
 
 # make directory for output
 mkdir -p $wd/vcf_output
 
-for bam_file in $bam_files/mapped/*sorted.bam
+# call SNPs using FreeBayes
+freebayes -f $ref_phi2 -p 1 $wd/bam_files/phage_2_sorted.bam > $wd/vcf_output/phage_2.vcf
+        
+# keep only snps with a QUAL score > 20, based on recommendation of Erik Garrison #https://www.biostars.org/p/71299/
+vcffilter -f "QUAL > 20" $wd/vcf_output/phage_2.vcf > $wd/vcf_output/phage_2_filter.vcf
 
-    do
-        echo $bam_file
-        stub=${bam_file%.bam}
-        stub2=$(basename ${bam_file%.bam})
-
-        # call SNPs using FreeBayes, set ploidy to 1
-        freebayes -f $ref_phi2  -p 1 $bam_file > $wd/vcf_output/${stub2}_freebayes.vcf
-        # keep only snps with a QUAL score > 20, based on recommendation of Erik Garrison
-        #https://www.biostars.org/p/71299/
-        vcffilter -f "QUAL > 20" $wd/vcf_output/${stub2}_freebayes.vcf > $wd/vcf_output/${stub2}_freebayes_filter.vcf
-done
+# phage_1_filter.vcf and phage_2_filter.vcf become phage_1.vcf and phage_2.vcf in the GitHub repository
